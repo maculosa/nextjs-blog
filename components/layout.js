e74d4421005c7e607677a1c2eeef4703315eaf76
lqf <maculosa@163.com>
2025-02-05 17:04:57 +0800

    feat: add tailwindcss

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,67 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-export default function Layout({ children }) {
+const name = 'maculosa'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
-            {children}
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://vercel.com/api/www/avatar/04f3e834ac5e37334977a76e5d6ada416b89e6bc?s=160`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt={""}
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Image 
+                                priority
+                                src="/images/profile.jpg"
+                                className={utilStyles.borderCircle}
+                                height={108}
+                                width={108}
+                                alt={""}
+                            />
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/" className={utilStyles.colorInherit}>
+                                {name}
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">← Back to home</Link>
+                </div>
+            )}
         </div>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'light-blue': '#03A9F4',
+                'light-blue-hover': '#0288D1',
+                'light-blue-active': '#0277BD',
+                'light-blue-dark': '#01579B',
+                'light-blue-dark-hover': '#004C8C',
+                'light-blue-dark-active': '#003C6F',
+            }
+        }
+    },
+}
